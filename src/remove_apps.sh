18fd12ee9f8af5a6af831f0abe81dd51f35f9331
asandikci <git@aliberksandikci.com.tr>
2023-11-29 17:18:14 +0300

    refactor: make removal process in for loop

--- src/remove_apps.sh
+++ src/remove_apps.sh
@@ -15,17 +15,26 @@ NC='\033[0m \e[0m' # No Color, No Effect
 # BOLD='\033[1;97m'
 
 _start() {
-  echo -e "${CYAN}[ i ] Removing package: ${NC} $1"
+  echo -e "${CYAN}[ i ] Removing package:${NC}$1"
 }
 _done() {
-  echo -e "${GREEN}[ ✔ ] Successfully removed package: ${NC} $1"
+  echo -e "${GREEN}[ ✔ ] Successfully removed package:${NC}$1"
 }
 
-_start "gimp"
-sudo apt purge gimp
-_done "gimp"
+#removes app with package name
+_remove() {
+  _start "$1"
+  sudo apt purge "$1"
+  _done "$1"
+}
+
+APPS_TO_BE_REMOVED=("gimp" "icedtea-netx")
+
+for item in "${APPS_TO_BE_REMOVED[@]}"
+do
+    _remove "$item"
+done
 
 # TODO Uygulamayı silmeden önce uygulamanın olup olmadığı kontrol edilebilir
 # TODO error handling, silme işlemi hata verirse veya kabuk hata kodu dönerse kullanıcı bilgilendirilmeli
 # TODO çok fazla yazı gözükmemesi adına _start komutu verbose değişkenine bağlanabilir, varsayılan olarak gizlenir
-# TODO basit bir komutla tek tek silinmesi gereken uygulamalar için array'e atıp bir for döngüsü açılabilir. Fazla koddan kaçınılmış olur
