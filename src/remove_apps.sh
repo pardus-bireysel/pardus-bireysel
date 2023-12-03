2226dcd759ae2644d02268530fbdbd1e96172e75
asandikci <git@aliberksandikci.com.tr>
2023-12-03 03:35:32 +0300

    feat: remove common, xfce,  gnome apps separately

--- src/remove_apps.sh
+++ src/remove_apps.sh
@@ -5,27 +5,53 @@
 
 ### Remove unnecessary applications from system ###
 
+# REVIEW Shellcheck / Bash IDE extensions is not working when sourcing from another dir
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source "$DIR/../common.sh"
 
 #removes app with package name
 _remove() {
-  _log "$1" verbose
+  _log "Uygulama siliniyor: $1" verbose
   sudo apt purge "$1" -y
-  _log "$1" ok
+  _log "Uygulama silindi: $1" ok
 }
 
-APPS_TO_BE_REMOVED=("gimp" "icedtea-netx" "thunar" "mousepad")
-KEEP_APPS_IN_XFCE=("thunar" "mousepad")
+# SEE https://tracker.pardus.org.tr/ for packages
+COMMON_APPS=("gimp" "icedtea-netx" "audacious" "firefox-esr" "libreoffice")
+XFCE_APPS=("thunar" "mousepad" "ristretto" "xfburn"
+  "xfce4" "xfce4-sensors-plugin"
+  "pardus-xfce-desktop"
+  "pardus-xfce-greeter"
+  "pardus-xfce-gtk-theme"
+  "pardus-xfce-icon-theme"
+  "pardus-xfce-icon-theme-metal" # only in Pardus 19
+  "pardus-xfce-live-settings"
+  "pardus-xfce-settings"
+  "pardus-xfce-tweaks"
+)
+GNOME_APPS=("nautilus" "gedit" "brasero"
+  "malcontent" "malcontent-gui"
+  "pardus-gnome-desktop"
+  "pardus-gnome-greeter"
+  "pardus-gnome-settings"
+  "pardus-gnome-shortcuts"
+  "gnome-shell-extension-arc-menu"
+)
 
-for item in "${APPS_TO_BE_REMOVED[@]}"; do
+_logconf "DESKTOP_ENVIRONMENT"
+
+for item in "${COMMON_APPS[@]}"; do
   _remove "$item"
 done
 
-_log "$DESKTOP_ENVIRONMENT" verbose
-## FIXME !!! DESKTOP_ENVRONMENT gets defaults value back after common.sh sourced again in 9th line
-## Possible solution1: use config file (read/write)
-## Possible solution2: Move changeble variable to another file and try to make it wirt export variables
+if [[ $(_gc "DESKTOP_ENVIRONMENT") != "xfce" ]]; then
+  for item in "${XFCE_APPS[@]}"; do
+    _remove "$item"
+  done
+fi
 
-# TODO Uygulamayı silmeden önce uygulamanın olup olmadığı kontrol edilebilir
-# TODO error handling, silme işlemi hata verirse veya kabuk hata kodu dönerse kullanıcı bilgilendirilmeli
+if [[ $(_gc "DESKTOP_ENVIRONMENT") != "gnome" ]]; then
+  for item in "${GNOME_APPS[@]}"; do
+    _remove "$item"
+  done
+fi
