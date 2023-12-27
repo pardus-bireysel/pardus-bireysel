f76c5d1c99b5730f953552f4e8eb2d6826826029
asandikci <git@aliberksandikci.com.tr>
2023-12-27 23:02:25 +0300

    fix: remove apps and xfce after kde installation for ux

--- src/kde_install.sh
+++ src/kde_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# sudo apt-get -y install kde-plasma-desktop
-# Komutun Kde plasma ortamı uygulamalarını indirip indirmediği belirlenmesi gerek.
+# 2023 🄯 Pardus Bireysel Contributors
+# https://github.com/pardus-bireysel/pardus-bireysel
 
 # Plasma masaüstü ortamının yüklenmesi ve ilgili KDE uygulamalarının yüklenmesi
+
+# REVIEW Komutun Kde plasma ortamı uygulamalarını indirip indirmediği belirlenmesi gerek.
+sudo apt-get -y install kde-plasma-desktop
