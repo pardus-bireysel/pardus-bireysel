3cae6524aafa64a9546ff11a46a25ab23868c3ee
Muhammed Emin Kola <152064891+mhmmdeminkl@users.noreply.github.com>
2023-12-27 06:01:28 +0300

    Update kde_install.sh
    Komut ve işlevi hakkında deneme amaçlı commit

--- src/kde_install.sh
+++ src/kde_install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# sudo apt-get -y install kde-plasma-desktop
+# Komutun Kde plasma ortamı uygulamalarını indirip indirmediği belirlenmesi gerek.
+
 # Plasma masaüstü ortamının yüklenmesi ve ilgili KDE uygulamalarının yüklenmesi
